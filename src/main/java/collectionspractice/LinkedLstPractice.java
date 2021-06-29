c649c57ebcf6b9316c4b6ae867fdff7a4e2464f8
shyam <shyam.mh07@gmail.com>
2021-06-29 22:34:41 +0530

    arraylistpractice commit

---
+++ src/main/java/collectionspractice/LinkedLstPractice.java
@@ -0,0 +1,17 @@
+package collectionspractice;
+
+import org.junit.Test;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedLstPractice {
+
+    @Test
+    public void linkdList() {
+        List list = new LinkedList();
+        list.add("as");
+        System.out.println(list);
+
+
+    }
+}
